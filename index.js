f886ee8aba2f8d5219aec209fa9a670d576fdf6a
יקיר יקיר <yohananyakir@gmail.com>
2025-02-23 02:37:28 +0200

    add comments

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
+// contain all useres.
+// Alternatively you can use the localStorage.
 let users = [];
 
+//create default admin user
 let adminUser = {
     username: "admin",
     email: "admin@admin.co.il",
     password: "admin"
 }
 
+// push admin user into users list
 users.push(adminUser);
 
+// sign-out page demostrate also the main page
 signOutHnadler();
 
+// display sing-in page
 function signInManuHandler() {
 
+    // get the container div.
     const container = document.getElementById("container");
+    // get the sign-in template
     const signInTemplate = document.getElementById("sign-in-template");
+    //clone the sign-in template content
     const signInClone = signInTemplate.content.cloneNode(true); // true for copy decendences too.
-    
-    container.innerHTML = '';
-    container.appendChild(signInClone);
+
+    container.innerHTML = ''; //reset page content
+    container.appendChild(signInClone); // add new content (the cloned sign-in template content).
 }
 
+// sign-in logic
 function signInButtonHandler() {
-    const username = document.getElementById("username");
-    const password = document.getElementById("password");
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    const user = users.find(x => x.username === username.value);
+    const user = users.find(x => x.username === username);
 
     if (user) {
-        if (password.value === user.password) {
+        if (password === user.password) {
             alert("Welcome ", username.innerText, " !");
 
             const container = document.getElementById("container");
@@ -47,6 +57,7 @@ function signInButtonHandler() {
 
 }
 
+// display sign-up page
 function signUpManuHandler() {
     const container = document.getElementById("container");
     const signUpTemplate = document.getElementById("sign-up-template");
@@ -56,6 +67,7 @@ function signUpManuHandler() {
     container.appendChild(signUpClone);
 }
 
+// sign-up logic
 function signUpSubmitHandler() {
     const username = document.getElementById("uname").value;
     const email = document.getElementById("email").value;
@@ -64,20 +76,27 @@ function signUpSubmitHandler() {
 
 
     if (password === rePassword) {
+
+        // cretae new user
         let newUser = {
             username: username,
             email: email,
             password: password
         };
 
+        // add the new user into users array
         users.push(newUser);
+
         alert("You are redirecting to sign-in page!");
+        // display the sign-in page
         signInManuHandler();
+
     } else {
         alert("Password don't match! please try again.");
     }
 }
 
+// sign-out page
 function signOutHnadler() {
     const container = document.getElementById("container");
 
